4701a9a19c76bbb746b2f1d9985db31e8568c576
Wietse Schmitt <w.schmitt@evidos.nl>
2017-10-12 14:32:36 +0200

    fix for EVENTSTORE_INT_IP environment issue

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-if [ ! -z ${EVENTSTORE_CLUSTER_SIZE+x} ]
+if [ ! -z ${EVENTSTORE_CLUSTER_SIZE+x} ] && [ -z ${EVENTSTORE_INT_IP} ]
 then
     export EVENTSTORE_INT_IP=`ip addr show eth0|grep '\/24'|awk '/inet / {print $2}'|sed -e 's/\/.*//'`
 fi
