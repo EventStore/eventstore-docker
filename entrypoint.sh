15ef0b436720e79bf0c142a719a0d758a2fd68b4
Jason Baker <jason.baker@spok.com>
2017-11-15 13:06:09 -0600

    Update entrypoint.sh
    startup script assumes eventstore container is deployed on a /24 network

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 
 if [ ! -z ${EVENTSTORE_CLUSTER_SIZE+x} ]
 then
-    export EVENTSTORE_INT_IP=`ip addr show eth0|grep '\/24'|awk '/inet / {print $2}'|sed -e 's/\/.*//'`
+    export EVENTSTORE_INT_IP=`ip addr show eth0|grep -E '(\/[0-9][0-9])'|awk '/inet / {print $2}'|sed -e 's/\/.*//'`
 fi
 
 exec eventstored
